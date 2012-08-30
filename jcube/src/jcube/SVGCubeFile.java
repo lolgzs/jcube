05ca117f09c0e631c2b2b68e1d644b2437ca73d9
Laurent Laffont <laurent.laffont@gmail.com>
2012-08-30 22:21:25 +0200

    Première transformation du SVG

--- jcube/src/jcube/SVGCubeFile.java
+++ jcube/src/jcube/SVGCubeFile.java
@@ -1,5 +1,26 @@
 package jcube;
 
+import java.io.IOException;
+import java.io.StringWriter;
+
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpression;
+import javax.xml.xpath.XPathExpressionException;
+import javax.xml.xpath.XPathFactory;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
 public class SVGCubeFile {
 	private String xml;
 	
@@ -12,8 +33,22 @@ public class SVGCubeFile {
 	}
 
 
-	public String content() {
-		return this.xml;
+	public String content() throws SAXException, IOException, ParserConfigurationException, XPathExpressionException, TransformerException {
+		Document doc = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(this.xml);
+		XPath xpath = XPathFactory.newInstance().newXPath();
+		XPathExpression expr = xpath.compile("//tspan[contains(text(), \"BLOCK1\")]");
+		NodeList nodes = (NodeList)expr.evaluate(doc, XPathConstants.NODESET);
+		nodes.item(0).setTextContent("Create & clone");
+		
+		Transformer transformer = TransformerFactory.newInstance().newTransformer();
+		transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+
+		StreamResult result = new StreamResult(new StringWriter());
+		DOMSource source = new DOMSource(doc);
+		transformer.transform(source, result);
+		
+		String output = result.getWriter().toString();
+		return output;
 	}
 
 }
--- jcube/tests/jcube/CubeSVGFromFacesWithGitCheat.java
+++ jcube/tests/jcube/CubeSVGFromFacesWithGitCheat.java
@@ -3,10 +3,12 @@ package jcube;
 import static org.junit.Assert.*;
 
 import java.io.IOException;
+import java.io.StringReader;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.TransformerException;
 import javax.xml.xpath.XPath;
 import javax.xml.xpath.XPathConstants;
 import javax.xml.xpath.XPathExpression;
@@ -17,30 +19,25 @@ import org.junit.Before;
 import org.junit.Test;
 import org.w3c.dom.Document;
 import org.w3c.dom.NodeList;
+import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
 
 public class CubeSVGFromFacesWithGitCheat {
 	private String svg;
+	private Document doc;
 	
 	@Before
-	public void generateSVGFromGitCheat() throws IOException {
+	public void generateSVGFromGitCheat() throws IOException, ParserConfigurationException, SAXException, XPathExpressionException, TransformerException {
 		Faces faces = Faces.loadFile("templates/git-cheat.txt");
 		this.svg = SVGCubeFile.fusion(faces, "templates/emacs-cube.svg").content();
+		this.doc =  DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(new InputSource(new StringReader(this.svg)));
 	}
 
 	@Test
-	public void svgShouldBeAnXMLFile() throws ParserConfigurationException, SAXException, IOException {
-		DocumentBuilderFactory domFactory = DocumentBuilderFactory.newInstance();
-		DocumentBuilder builder = domFactory.newDocumentBuilder();
-		builder.parse(this.svg);
-	}
-	
-	@Test
-	public void aTagTSpanShouldContainsBLOCK1() throws SAXException, IOException, ParserConfigurationException, XPathExpressionException {
-		Document doc = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(this.svg);
+	public void aTagTSpanShouldContainsCreateAndClone() throws XPathExpressionException {
 		XPath xpath = XPathFactory.newInstance().newXPath();
-		XPathExpression expr = xpath.compile("//tspan[contains(text(), \"BLOCK1\")]");
-		NodeList nodes = (NodeList)expr.evaluate(doc, XPathConstants.NODESET);
+		XPathExpression expr = xpath.compile("//tspan[contains(text(), \"Create & clone\")]");
+		NodeList nodes = (NodeList)expr.evaluate(this.doc, XPathConstants.NODESET);
 		assertEquals(1, nodes.getLength());
 	}
 }

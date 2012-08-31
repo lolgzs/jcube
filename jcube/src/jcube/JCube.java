f229b54e298a1eaab91a1ae8d933f1264ff3b7d1
Laurent Laffont <laurent.laffont@gmail.com>
2012-08-31 22:41:36 +0200

    Ajout d'un main pour tester la fusion en réel

---
+++ jcube/src/jcube/JCube.java
@@ -0,0 +1,25 @@
+package jcube;
+
+import java.io.IOException;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.xpath.XPathExpressionException;
+
+import org.xml.sax.SAXException;
+
+public class JCube {
+
+	/**
+	 * @param args
+	 * @throws TransformerException 
+	 * @throws ParserConfigurationException 
+	 * @throws SAXException 
+	 * @throws IOException 
+	 * @throws XPathExpressionException 
+	 */
+	public static void main(String[] args) throws XPathExpressionException, IOException, SAXException, ParserConfigurationException, TransformerException {
+		System.out.println((new SVGCubeFile()).fusion(args[0], args[1]));
+	}
+
+}

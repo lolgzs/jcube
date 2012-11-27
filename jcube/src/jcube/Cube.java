538776e37c861a7d250b59af92dfd5c0ac5874ff
Laurent Laffont <laurent.laffont@gmail.com>
2012-11-27 15:44:16 +0100

    Cheat + Face

--- jcube/src/jcube/Cube.java
+++ jcube/src/jcube/Cube.java
@@ -4,27 +4,38 @@ import java.io.IOException;
 import org.w3c.dom.Element;
 
 public class Cube {
-	private String faceOne;
+	private Face face;
 
 	public static Cube fromTextFile(String filepath) throws IOException {
 		return (new CubeTextFile(filepath)).newCube();
 	}
 
 	public Cube addFace(String string) {
-		this.faceOne = string;
+		this.newFace(string);
 		return this;
 	}
 	
 	public boolean equals(Object other) {
 		Cube otherCube = (Cube)other;
-		return this.faceOne.equals(otherCube.faceOne);
+		return this.face.equals(otherCube.face);
 	}
 
 	public XMLDocument populateSVGFile(String filepath) throws Exception {
 		XMLDocument sourceSVG = (new XMLDocument()).loadXMLFile(filepath);
 		Element blocOneNode = sourceSVG.getFirstNodeFromXPath("//tspan[contains(text(), '$BLOCK1')]");
-		blocOneNode.setTextContent(this.faceOne);
+			
+		IFaceWriter faceWriter = new ElementFaceWriter(blocOneNode);
+		this.face.writeOn(faceWriter);
+		
+		IFaceWriter faceWriter = new JPegFaceWriter("mon.jpeg");
+		this.face.writeOn(faceWriter);
+		
 		return sourceSVG;
+	}
+
+	public Face newFace(String title) {
+		this.face =  new Face(title);
+		return this.face;
 	}
 
 }
--- jcube/test/jcube/CubeEqualityTest.java
+++ jcube/test/jcube/CubeEqualityTest.java
@@ -12,5 +12,30 @@ public class CubeEqualityTest {
 		Cube cubeSVN = (new Cube()).addFace("SVN");
 		assertFalse(cubeGit.equals(cubeSVN));
 	}
+	
+	@Test
+	public void twoCubesWithOneFaceAndDifferentCheatsShouldNotBeEqual() {
+		Cube cubeGit1 = (new Cube());
+		cubeGit1.newFace("Git").newCheat("git clone", "clone repository");
+		
+		Cube cubeGit2 = (new Cube());
+		cubeGit2.newFace("Git").newCheat("git pull", "get and merge latest changes");
+		
+		
+		assertFalse(cubeGit1.equals(cubeGit2));
+	}
+	
+	
+	@Test
+	public void twoCubesWithOneFaceAndSameCheatsShouldBeEqual() {
+		Cube cubeGit1 = (new Cube());
+		cubeGit1.newFace("Git").newCheat("git clone", "clone repository");
+		
+		Cube cubeGit2 = (new Cube());
+		cubeGit2.newFace("Git").newCheat("git clone", "clone repository");
+		
+		
+		assertEquals(cubeGit1, cubeGit2);
+	}
 
 }
